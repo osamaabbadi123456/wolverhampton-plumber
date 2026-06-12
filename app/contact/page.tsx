29d424d004ce0f261752cbf06c9d3d0318c2ee42
Osama <osamaabbadi123456@gmail.com>
2026-06-12 15:34:41 +0300

    Fix Netlify form setup

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -60,8 +60,6 @@ export default function ContactPage() {
               className="contactForm"
               name="plumbing-enquiry"
               method="POST"
-              data-netlify="true"
-              netlify-honeypot="bot-field"
               action="/thank-you"
             >
               <input type="hidden" name="form-name" value="plumbing-enquiry" />
